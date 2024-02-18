4b22d8f5b33ceae611cd2775f86050080f1e9a8c
Blaaaxo <fernandezb@tecnocomfenalco.edu.co>
2024-02-18 14:06:24 -0500

    404 not found

---
+++ app/not-found.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from "next/link"
+
+const NotFound = () => {
+  return (
+    <section>
+        <h1>404</h1>
+        <p>Pagina no encontrada</p>
+        <Link href="/">
+            Volver
+        </Link>
+    </section>
+  )
+}
+
+export default NotFound
