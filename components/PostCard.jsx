9d9063c1e6f8ab07c6a14fe4e9b2cdb767fd0abb
Blaaaxo <fernandezb@tecnocomfenalco.edu.co>
2024-02-18 16:57:27 -0500

    parametros en las urls

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,11 +1,14 @@
 "use client"
 import React from 'react'
+import Link from 'next/link'
 
 // RCC (react client component)
 function PostCard({ post }) {
     return (
         <div>
-            <h2>{post.id}. {post.title}</h2>
+            <Link href={`/posts/${post.id}`}>
+                <h2>{post.id}. {post.title}</h2>
+            </Link>
             <p>{post.body}</p>
             <button onClick={() => {
                 alert("click funcionando en el post numero " + post.id)
